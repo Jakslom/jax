d9cd27ff2c16b3bf21f29530eccf598d614b1979
Jakslom <59932266+Jakslom@users.noreply.github.com>
2020-01-23 18:29:09 +0100

    2

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 
 require(["esri/Map", "esri/views/MapView"],
     function(Map,MapView){
-        let map1 = new Map({basemap:"topo"});
+        let mapp = new Map({basemap:"topo"});
+        let map1 = new Map ({basemap:"satellite"});
+        let map2 = new Map ({basemap:"osm"})
+        let button1 = document.getElementById("bt1")
+        let button2 = document.getElementById("bt2");
+        let button3 = document.getElementById("bt3");
 
         let mapContainer = new MapView({
             container: "mapView",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
-            map: map1 , //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
+            map: mapp , //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
             zoom: 4,
+        })
+
+        button1.addEventListener('click',function(){
+            mapContainer.map = map1
+            mapContainer.zoom = 5;
+             })
+        button2.addEventListener('click',function(){
+            mapContainer.map = mapp;
+            mapContainer.zoom = 5;
+             })
+        button3.addEventListener('click',function(){
+            mapContainer.map = map2;
+            mapContainer.zoom = 5;
+             })
+        
             
-
-         }); 
-
-    })
+         });
